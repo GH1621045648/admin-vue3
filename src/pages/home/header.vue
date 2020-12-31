14dbe7809970bedd64a77dcb8ff07cdfdd6c2341
Conan Liu <conan.liu@cignacmb.com>
2020-12-31 17:17:03 +0800

    更改结构

--- src/pages/home/header.vue
+++ src/pages/home/header.vue
@@ -1,6 +1,15 @@
 <template>
+  <div class="icon">
+    <h1
+      class="fire"
+      :style="{'color':theme}"
+    >shuquanliu</h1>
+  </div>
   <Theme @change='changeTheme'></Theme>
-  <div class="favicon">
+  <div
+    class="favicon"
+    style=" background: linear-gradient(red, blue);"
+  >
     a
   </div>
 </template>
@@ -25,6 +34,16 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.fire {
+  height: 88px;
+  width: 200px;
+  line-height: 72px;
+  text-align: center;
+  font-family: 'Comic Sans MS';
+  font-size: 36px;
+  color: white;
+  text-shadow: 0 0 20px #fefcc9, 10px -10px 30px #feec85, 0px 20px 5px #ffae34, 20px -40px 50px #ec760c, -20px -60px 60px #cd4606, 0 -80px 70px #973716, 10px -90px 80px #451b0e;
+}
 .favicon {
   width: 88px;
   height: 88px;
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,29 +1,18 @@
 <template>
-  <div class="sideBar">
+  <div
+    id="header"
+    :style="{'border-bottom':`1px solid ${theme}`}"
+  >
+    <Header></Header>
+  </div>
+  <div id="main">
     <div
-      class="icon"
-      :style="{'border-bottom':`1px solid ${theme}`}"
-    >
-      <h1
-        id="fire"
-        :style="{'color':theme}"
-      >shuquanliu</h1>
-    </div>
-    <div
-      class="aside"
+      id="aside"
       :style="{'border-right':`1px solid ${theme}`}"
     >
       <SideBar></SideBar>
     </div>
-  </div>
-  <div class="content">
-    <div
-      class="header"
-      :style="{'border-bottom':`1px solid ${theme}`}"
-    >
-      <Header></Header>
-    </div>
-    <div class="container">
+    <div id="content">
       <router-view></router-view>
     </div>
   </div>
@@ -49,15 +38,3 @@ export default {
   mounted() {}
 };
 </script>
-
-<style lang="less" scoped>
-#fire {
-  height: 100%;
-  line-height: 72px;
-  text-align: center;
-  font-family: 'Comic Sans MS';
-  font-size: 36px;
-  color: white;
-  text-shadow: 0 0 20px #fefcc9, 10px -10px 30px #feec85, 0px 20px 5px #ffae34, 20px -40px 50px #ec760c, -20px -60px 60px #cd4606, 0 -80px 70px #973716, 10px -90px 80px #451b0e;
-}
-</style>
